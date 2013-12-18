adbe2dbd96feff155fb701be3c67435f1616f318
Martini Tommaso 1081580/TC <martinit@pcda28.dei.unipd.it>
2013-12-18 10:19:12 +0100

    sviluppi sulla routine per accertarsi che i nodi siano collegati

--- Prince/src/com/prince/BootstrapNode.java
+++ Prince/src/com/prince/BootstrapNode.java
@@ -4,15 +4,17 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.PrintStream;
+import java.net.DatagramPacket;
+import java.net.DatagramSocket;
 import java.net.InetAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.SocketException;
+import java.net.UnknownHostException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Timer;
 import java.util.TimerTask;
-import java.util.logging.Handler;
-import java.util.logging.Logger;
 
 /* Database:
  * prova
@@ -28,34 +30,51 @@ public class BootstrapNode {
 	//	Listening ports
 	private static final int PORT_JOINED_NODE = 8001;
 	private static final int PORT_DEPARTED_NODE = 8002;
+	private static final int PORT_ALIVE_NODE = 8004;
 
 	//	Speaking ports
 	private static final int PORT_ASK_ALIVE_NODES = 8000;
-	private static final int PORT_TABLE = 8003;
+	//	private static final int PORT_TABLE = 8003;
 
 	//	Testing ports
 	private static final int PORT_TESTING = 7777;
 
-	private static final String LOGGER_NAME = "Bootstrap";
 	private static final long DELAY_ASK_FOR_ALIVE = 1000;
 	private static final long PERIOD_ASK_FOR_ALIVE = 1000;
+
+	//	States
+	private static final int STATE_RUNNING = 0;
+	private static final int STATE_ROLL_CALLING = 1;
+
+	//	Subject states ("stati del suddito")
+	private static final int SUBJECT_STATE_MISSING = 0;
+	private static final int SUBJECT_STATE_ALIVE = 1;
+	private static final int SUBJECT_STATE_DEAD = 2;
+
+	private static int currentState;
 
 	private static int counter = 0;
 	private static int node_id = 0;
 
+	//	ServerSockets
 	private ServerSocket serverSocket;	
-	private ServerSocket newNodeListener;
+	private ServerSocket joinedNodeListener;
 	private ServerSocket departedNodeListener;
+	private ServerSocket aliveNodeListener;
 
-	private Logger logger;
+	//	Listening threads
+	JoinedNodeListenerThread joinedNodeListenerThread;
+	DepartedNodeListenerThread departedNodeListenerThread;
+	AliveNodeListenerThread aliveNodeListenerThread;
 
 	private Map<Integer, Node> nodes;
-	
-	private Handler handler;	// DA USARE?? serve per il timer della richiesta alive
+	private Map<Integer, Integer> rollCallRegister;	// "registro per fare l'appello"
 
 	private BootstrapNode() {
-		logger = Logger.getLogger(LOGGER_NAME);
 		nodes = new HashMap<Integer, BootstrapNode.Node>();
+		rollCallRegister = new HashMap<Integer, Integer>();
+
+		currentState = STATE_RUNNING;
 
 		try {
 			serverSocket = new ServerSocket(PORT_TESTING);
@@ -67,13 +86,16 @@ public class BootstrapNode {
 		Timer timer = new Timer();
 		TimerTask task = new AliveAskerTask();
 		timer.schedule(task, DELAY_ASK_FOR_ALIVE, PERIOD_ASK_FOR_ALIVE);
-		
-		//		Faccio partire tutti i thread incaricati di ascoltare
-		JoinedNodeListenerThread joinedNodeListenerThread = new JoinedNodeListenerThread();
+
+		//		Start listening threads
+		joinedNodeListenerThread = new JoinedNodeListenerThread();
 		joinedNodeListenerThread.start();
 
-		DepartedNodeListenerThread departedNodeListenerThread = new DepartedNodeListenerThread();
+		departedNodeListenerThread = new DepartedNodeListenerThread();
 		departedNodeListenerThread.start();
+
+		aliveNodeListenerThread = new AliveNodeListenerThread();
+		aliveNodeListenerThread.start();
 	}	// BootstrapNode()
 
 	public static void main(String[] args) {
@@ -96,8 +118,8 @@ public class BootstrapNode {
 			}
 		}
 	}	// runBootstrap()
-	
-//	W@erraid@numeronodiattivinellarete@ip#erraid%ip#erraid%ip#erraid%...%
+
+	//	W@erraid@numeronodiattivinellarete@ip#erraid%ip#erraid%ip#erraid%...%
 	private String getNodesMapToString() {
 		String mapToString = String.valueOf(nodes.size()) + "@";
 		for(Map.Entry<Integer, Node> entry : nodes.entrySet()) {
@@ -151,29 +173,41 @@ public class BootstrapNode {
 
 		@Override
 		public void run() {
+			for (Map.Entry<Integer, Integer> entry : rollCallRegister.entrySet()) {
+				entry.setValue(SUBJECT_STATE_MISSING);
+			}
 			AliveAskerThread aliveAskerThread = new AliveAskerThread();
 			aliveAskerThread.start();
+			//			try {
+			//				notifiedAliveNodeListener = new ServerSocket(PORT_ALIVE_NODE);
+			//				while (true) {
+			//					
+			//				}
+			//				Socket newSocket = notifiedAliveNodeListener.accept();
+			//			} catch (IOException e) {
+			//				e.printStackTrace();
+			//			} 
+			//			
+			//			NotifiedAliveNodeThread notifiedAliveNodeThread = new NotifiedAliveNodeThread();
+			//			notifiedAliveNodeThread.start();
 		}
 	}
-	
+
 	/*
-	 * 	Threads
+	 * ****************************************************************************
+	 * START THREADS
 	 */
 
-	private class AliveAskerThread extends Thread {
-
-		@Override
-		public void run() {
-			System.out.println("time!");
-		}
-	}
+	/*
+	 * 	Listening Threads
+	 */
 
 	private class JoinedNodeListenerThread extends Thread {
 
 		public JoinedNodeListenerThread() {
 			super();
 			try {
-				newNodeListener = new ServerSocket(PORT_JOINED_NODE);
+				joinedNodeListener = new ServerSocket(PORT_JOINED_NODE);
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
@@ -181,21 +215,21 @@ public class BootstrapNode {
 
 		@Override
 		public void run() {
-			super.run();
 			//			logger.log(Level.INFO, "Avviato JoinedNodeListenerThread");
 			System.out.println("Avviato JoinedNodeListenerThread");
 
 			while (true) {
+				super.run();
 				try {
-					Socket socket = newNodeListener.accept();
-					AddNodeThread addNodeThread = new AddNodeThread(socket);
+					Socket newSocket = joinedNodeListener.accept();
+					AddNodeThread addNodeThread = new AddNodeThread(newSocket);
 					addNodeThread.start();
 				} catch (IOException e) {
 					e.printStackTrace();
 				}
 			}
 		}		
-	}
+	}	// JoinedNodeListenerThread
 
 	private class DepartedNodeListenerThread extends Thread {
 
@@ -224,20 +258,73 @@ public class BootstrapNode {
 				}
 			}
 		}	
-	}
+	}	// DepartedNodeListenerThread
 
-	private class NotifiedAliveNodeThread extends Thread {
+	private class AliveNodeListenerThread extends Thread {
+
+		public AliveNodeListenerThread() {
+			super();
+			try {
+				aliveNodeListener = new ServerSocket(PORT_ALIVE_NODE);
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
 
 		@Override
 		public void run() {
-			// TODO Auto-generated method stub
 			super.run();
+			System.out.println("Avviato AliveNodeListenerThread");
+
+			while (true) {
+				try {
+					Socket newSocket = aliveNodeListener.accept();
+					NotifiedAliveNodeThread notifiedAliveNodeThread = new NotifiedAliveNodeThread(newSocket);
+					notifiedAliveNodeThread.start();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
 		}
-	}
+	}	// AliveNodeListenerThread
+
+	/*
+	 * Speaking Threads
+	 */
+
+	private class AliveAskerThread extends Thread {
+
+		@Override
+		public void run() {
+			super.run();
+			try {
+				DatagramSocket datagramSocket = new DatagramSocket(PORT_ASK_ALIVE_NODES);
+				DatagramPacket datagramPacket;
+				byte[] msg = (new String("?")).getBytes();
+				for(Map.Entry<Integer, Node> entry : nodes.entrySet()) {
+					Node currentNode = entry.getValue();
+					datagramPacket = new DatagramPacket(msg, msg.length, InetAddress.getByName(currentNode.getIP_ADDRESS()), PORT_ASK_ALIVE_NODES);
+					datagramSocket.send(datagramPacket);
+				}
+				datagramSocket.close();
+			} catch (SocketException e) {
+				e.printStackTrace();
+			} catch (UnknownHostException e) {
+				e.printStackTrace();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+
+		}
+	}	// AliveAskerThread
+
+	/*
+	 * Action Threads
+	 */
 
 	private class AddNodeThread extends Thread {
 
-		Socket socket;
+		private Socket socket;
 
 		public AddNodeThread(Socket newSocket) {
 			super();
@@ -246,7 +333,7 @@ public class BootstrapNode {
 
 		@Override
 		public void run() {
-
+			super.run();
 			String msgFromNode = null;
 			try {
 				BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -255,7 +342,7 @@ public class BootstrapNode {
 				e.printStackTrace();
 			}
 
-			if (msgFromNode.length() == 0 || !msgFromNode.equalsIgnoreCase("J")) {
+			if (msgFromNode == null || msgFromNode.length() == 0 || !msgFromNode.equalsIgnoreCase("J")) {
 				System.out.println("Il messaggio del client che chiede di aggiungersi alla rete e' vuoto o diverso da \'J\'");
 			} else {
 
@@ -271,11 +358,10 @@ public class BootstrapNode {
 					int currentId = node_id++;
 					Node node = new Node(currentId, ipString);
 					nodes.put(currentId, node);
+					rollCallRegister.put(currentId, SUBJECT_STATE_ALIVE);
 
 					System.out.println("Inserito nodo con id " + currentId);
-//									TODO ora devo dire al nodo che id ha
-//					W@erraid@numeronodiattivinellarete@ip#erraid%
-					
+
 					try {
 						PrintStream toNode = new PrintStream(socket.getOutputStream());
 						String table = "W@" + currentId + "@" + getNodesMapToString() + "\n";
@@ -286,11 +372,11 @@ public class BootstrapNode {
 				}	
 			}
 		}	
-	}
+	}	// AddNodeThread
 
 	private class DeleteNodeThread extends Thread {
 
-		Socket socket;
+		private Socket socket;
 
 		public DeleteNodeThread(Socket newSocket) {
 			super();
@@ -299,6 +385,7 @@ public class BootstrapNode {
 
 		@Override
 		public void run() {
+			super.run();
 			String msgFromNode = null;
 			try {
 				BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
@@ -306,22 +393,61 @@ public class BootstrapNode {
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			if (msgFromNode.length() == 0 || !msgFromNode.substring(0, 1).equalsIgnoreCase("E")) {
-//				TODO lanciare un'eccezione perche' ho ricevuto un indirizzo vuoto!
+			if (msgFromNode == null || msgFromNode.length() == 0 || !msgFromNode.substring(0, 1).equalsIgnoreCase("E")) {
+				// TODO lanciare un'eccezione perche' ho ricevuto un indirizzo vuoto!
 				System.out.println("Il messaggio del client che chiede di aggiungersi alla rete e' vuoto o diverso da \'J\'");
 				System.out.println(msgFromNode);
 			} else {
-//				messaggio nella forma E@erraid\n
+				// messaggio nella forma E@erraid\n
 				int identifier = Integer.parseInt(msgFromNode.substring(2));
 				if (nodes.remove(identifier) != null) {
 					System.out.println("Nodo " + identifier + " rimosso dalla rete");
+					rollCallRegister.remove(identifier);
+					//rollCallRegister.put(identifier, SUBJECT_STATE_DEAD);
 				} else {
 					System.out.println("Il nodo " + identifier + " non e' presente nella rete");
-//				TODO devo avvertire tutti i nodi che il nodo corrente ha lasciato la rete
+					//					TODO devo avvertire tutti i nodi che il nodo corrente ha lasciato la rete
 				}
 			}
 		}	
-	}
+	}	// DeleteNodeThread
+
+	private class NotifiedAliveNodeThread extends Thread {
+
+		private Socket socket;
+
+		public NotifiedAliveNodeThread(Socket newSocket) {
+			super();
+			socket = newSocket;
+		}
+
+		@Override
+		public void run() {
+			super.run();
+			String msgFromNode = null;
+			try {
+				//				Message in the form: !@erraid
+				BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+				msgFromNode = bufferedReader.readLine();
+				if (msgFromNode == null || msgFromNode.length() == 0 || !msgFromNode.substring(0, 1).equalsIgnoreCase("!")) {
+					// TODO lanciare un'eccezione perche' ho ricevuto un indirizzo vuoto!
+					System.out.println("Il messaggio del client che risponde di essere attivo e' vuoto o diverso da \'!\'");
+					System.out.println(msgFromNode);
+				} else {
+					String[] msgValues = msgFromNode.split("@");
+					int identifier = Integer.parseInt(msgValues[1]);
+					rollCallRegister.put(identifier, SUBJECT_STATE_ALIVE);
+				}
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+		}
+	}	// NotifiedAliveNodeThread
+
+	/*
+	 * ****************************************************************************
+	 * END THREADS
+	 */
 
 	/**
 	 * Un esemplare di questa classe e' un bean che contiene tutte le informazioni su ogni nodo entrato nella rete.
