7925cda5700ed68fea8fc78c5b75880cf4c363c8
tommasomartini <tommaso.martini91@gmail.com>
2013-12-28 23:00:21 +0100

    altre piccole correzioni e aggiunta di una classe di test

--- Prince/src/com/prince/BootstrapNode.java
+++ Prince/src/com/prince/BootstrapNode.java
@@ -470,8 +470,7 @@ public class BootstrapNode extends NewErraClient {
 			if (msgFromNode == null || msgFromNode.length() == 0 || !msgFromNode.substring(0, 1).equalsIgnoreCase("!")) {
 				System.err.println("Il messaggio del client che risponde di essere attivo e' vuoto o diverso da \'!\'");
 			} else {
-				String[] segments = msgFromNode.split(DELIMITER_AFTER_MSG_CHAR);
-				String ipAddress = segments[1];
+				String ipAddress = datagramPacket.getAddress().getHostAddress();
 				System.out.println("Subject " + ipAddress + " is alive!");
 				updateRegister(ipAddress, NodeState.NODE_STATE_ALIVE);
 			}
